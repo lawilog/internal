a16dee4e8d0764c249f4e316be3e1f4d66c147f3
Lars Winterfeld <lars.winterfeld@tu-ilmenau.de>
2013-08-09 14:58:29 +0200

    make combi_call Matlab-compatible (was Octave-only)

--- common/combi_call.m
+++ common/combi_call.m
@@ -1,4 +1,6 @@
-% combi_call calls callbackfunc with a vector of length n for each of the length(a)^n combinations of elements in a
+% function combi_call(a, n, callbackfunc)
+% calls callbackfunc with a vector of length n for each of the length(a)^n combinations of elements in a
+% e.g.: combi_call([11 22 33], 4, @disp)
 function combi_call(a, n, callbackfunc)
 	m = length(a);
 	c = ones(1, n);
@@ -7,12 +9,12 @@ function combi_call(a, n, callbackfunc)
 		callbackfunc( a(c) )
 		k = 1;
 		while k <= n
-			if ++c(k) <= m
+			c(k) = c(k) + 1;
+			if c(k) <= m
 				break
 			end
-			c(k++) = 1;
+			c(k) = 1;
+			k = k + 1;
 		end
 	end
 end
-
-% e.g.: combi_call([11 22 33], 4, @disp)
