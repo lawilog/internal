5f20e57e66dd87bc7e55088fac8ae0fc25b741c2
Lars Winterfeld <lars.winterfeld@tu-ilmenau.de>
2014-01-07 14:21:15 +0100

    add another testcase to function_interpolation_table.cpp

--- common/function_interpolation_table.cpp
+++ common/function_interpolation_table.cpp
@@ -220,6 +220,7 @@ int main()
 	ofstream d3("function_interpolation_table_f3.dat"); if(! d3) {cerr<<"Could not write file."<<endl; return 1;}
 	ofstream d4("function_interpolation_table_f4.dat"); if(! d4) {cerr<<"Could not write file."<<endl; return 1;}
 	ofstream d5("function_interpolation_table_f5.dat"); if(! d5) {cerr<<"Could not write file."<<endl; return 1;}
+	ofstream d6("function_interpolation_table_f6.dat"); if(! d6) {cerr<<"Could not write file."<<endl; return 1;}
 	
 	// closest
 	FunInterpolTable<closest> f0(sin);
@@ -241,7 +242,7 @@ int main()
 	
 	// precompute closest
 	FunInterpolTable<closest> f2(sin);
-	f2.precompute(0, 2*M_PI, 50);
+	f2.precompute(0, 2*M_PI, 10);
 	f2.setXPerc(0);
 	for(unsigned i = 0; i < 1000; ++i)
 	{
@@ -252,7 +253,7 @@ int main()
 	
 	// precompute linear
 	FunInterpolTable<linear> f3(sin);
-	f3.precompute(0, 2*M_PI, 50);
+	f3.precompute(0, 2*M_PI, 10);
 	f3.setXPerc(0);
 	for(unsigned i = 0; i < 1000; ++i)
 	{
@@ -271,7 +272,6 @@ int main()
 	}
 	cout<<"f4, cache: "<< f4.getCallCount() <<" calls, "<< f4.getEvalCount() <<" evals"<<endl;
 	
-	
 	// quadratic
 	FunInterpolTable<quadratic> f5(sin);
 	for(unsigned i = 0; i < 1000; ++i)
@@ -280,6 +280,19 @@ int main()
 		d5<< x <<"\t"<< f5(x) <<"\t"<< sin(x) <<endl;
 	}
 	cout<<"f5, random, quadratic: "<< f5.getCallCount() <<" calls, "<< f5.getEvalCount() <<" evals"<<endl;
+	
+	// precompute quadratic
+	FunInterpolTable<quadratic> f6(sin);
+	f6.precompute(0, 2*M_PI, 10);
+	f6.setXPerc(0);
+	for(unsigned i = 0; i < 1000; ++i)
+	{
+		double x = randuni();
+		d6<< x <<"\t"<< f6(x) <<"\t"<< sin(x) <<endl;
+	}
+	cout<<"f3, random, quadratic, precomputes: "<< f6.getCallCount() <<" calls, "<< f6.getEvalCount() <<" evals"<<endl;
+	
+	// gnuplot: plot "function_interpolation_table_f0.dat" w p title "random, closest", "function_interpolation_table_f1.dat" w p title "random, linear", "function_interpolation_table_f2.dat" w p title "random, closest, precomputes", "function_interpolation_table_f3.dat" w p title "random, linear, precomputes", "function_interpolation_table_f5.dat" w p title "random, quadratic", "function_interpolation_table_f6.dat" w p title "random, quadratic, precomputes", sin(x)
 	
 	return 0;
 }
