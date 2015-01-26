b0a2f5049948ba665f92270dd2ed4ffd5158273e
Lars Winterfeld <lars.winterfeld@tu-ilmenau.de>
2015-01-26 14:01:42 +0100

    allow comments (e.g. ./s2h.sh 12345 foo bar)

--- little_helpers/s2h.sh
+++ little_helpers/s2h.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-test -z "$1" && s=$(read x && echo "$x" ) || s="$1"
-echo -n "$s = "
-test $s -lt 60 && echo "${s}s" && exit
+s="$1"
+shift
+msg="${@}"
+test -z "$1" && read s msg
+printf "%9i = " "$s"
+test $s -lt 60 && echo -e "${s}s $msg\t${msg}" && exit
 min=$(( $s / 60 ))
 s=$(( $s % 60 ))
-test $min -lt 60 && echo "${min}min ${s}s" && exit
+test $min -lt 60 && echo -e "${min}min ${s}s\t${msg}" && exit
 h=$(( $min / 60 ))
 min=$(( $min % 60 ))
-test $h -lt 24 && echo "${h}h ${min}min ${s}s" && exit
+test $h -lt 24 && echo -e "${h}h ${min}min ${s}s\t${msg}" && exit
 d=$(( $h / 24 ))
 h=$(( $h % 24 ))
-test $d -lt 365 && echo "${d}d ${h}h ${min}min ${s}s" && exit
+test $d -lt 365 && echo -e "${d}d ${h}h ${min}min ${s}s\t${msg}" && exit
 a=$(( $d / 365 ))
 d=$(( $d % 365 ))
 echo "${a}a ${d}d ${h}h ${min}min ${s}s"
