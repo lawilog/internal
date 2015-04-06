67acca1a7b5cf2064db14b2dd310dada09441df8
Lars Winterfeld <lars@winterfeld.de>
2015-04-06 19:20:12 +0200

    invoke "mediainfo" tool and grep out some data

---
+++ little_helpers/mediainfo.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mediainfo "${@}" | egrep '^Video|^Audio|^Text|File size|Duration|Codec ID|Bit rate|Width|Height|Language' | awk '/:/{gsub(/ *:/, ":", $0); printf(" | %s", $0)}; !/:/{printf("\n%s\t", $0)} END{printf "\n"}'
