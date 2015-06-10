2ede9b74479263f7bdedfcac57263d69d8cb45ac
Lars Winterfeld <lars.winterfeld@tu-ilmenau.de>
2015-06-10 09:24:12 +0200

    much better string_similarity.sh (more options, e.g. compare by word or character), small improvement on imdb_search_film_title.sh

--- little_helpers/string_similarity.sh
+++ little_helpers/string_similarity.sh
@@ -1,13 +1,85 @@
 #!/bin/bash
+verbose=0
+printmode="a<b?a:b"
+cmpmode="w"
+while [[ $# > 0 ]]
+do
+	case "$1" in
+	-h|--help)
+		echo -e "$0 [options] \"string A\" \"string B\"\n" \
+			"options are:\n\n" \
+			"-h|--help: print this help\n\n" \
+			"compare modes [default: -byword]:\n" \
+			"-byword: use wdiff to compare strings word by word\n" \
+			"-bychar: use diff to compare strings character by character\n\n" \
+			"print modes [default: -min]:\n" \
+			"-A: print percentage of (words or characters) in string A that are also in string B\n" \
+			"-B: print percentage of (words or characters) in string B that are also in string A\n" \
+			"-min, -max, -avg: print minimum, maximum or average of -A and -B values"
+		exit 0
+	;;
+	
+	-v)
+		verbose=1
+		shift
+	;;
+	
+	-min)
+		printmode="a<b?a:b"
+		shift
+	;;
+	
+	-A)
+		printmode="a"
+		shift
+	;;
+	
+	-B)
+		printmode="b"
+		shift
+	;;
+	
+	-avg)
+		printmode="(a+b)/2"
+		shift
+	;;
+	
+	-max)
+		printmode="a>b?a:b"
+		shift
+	;;
+	
+	-byword)
+		cmpmode="w"
+		shift
+	;;
+	
+	-bychar)
+		cmpmode="d"
+		shift
+	;;
+	
+	*)
+		break
+	;;
+	
+	esac
+done
+
 fa=$(tempfile)
 fb=$(tempfile)
-
-# echo "$1" | sed 's/\(.\)/\1\n/g' > "$fa"
-# echo "$2" | sed 's/\(.\)/\1\n/g' > "$fb"
-# echo $(diff -di "$fa" "$fb" | egrep '^<|^>' | wc -l) $(cat "$fa" | wc -l) $(cat "$fb" | wc -l) | awk '{printf("%i\n", 100*(1-$1/($2+$3)))}'
-
-echo "$1" > "$fa"
-echo "$2" > "$fb"
-LC_ALL="C"  wdiff -si "$fa" "$fb" | awk '{e[x]=$5; x=!x;} END{printf("%i\n", e[0]<e[1]?e[0]:e[1])}' # (e[0]+e[1])/2
-
+if [ "$cmpmode" = "w" ]
+then
+	echo "$1" > "$fa"
+	echo "$2" > "$fb"
+	outp=$(LC_ALL="C"  wdiff -si "$fa" "$fb")
+	test $verbose -eq 1 && echo "$outp"
+	echo "$outp" | tail -2 | awk "{if(x++) b=1*\$5; else a=1*\$5;} END{printf(\"%i\\n\", $printmode)}"
+else
+	echo "$1" | sed 's/\(.\)/\1\n/g' > "$fa"
+	echo "$2" | sed 's/\(.\)/\1\n/g' > "$fb"
+	outp=$(diff -di "$fa" "$fb" | egrep '^<|^>')
+	test $verbose -eq 1 && echo "$outp"
+	echo $(echo "$outp" | egrep '^<' | wc -l) $(echo "$outp" | egrep '^>' | wc -l) $(cat "$fa" | wc -l) $(cat "$fb" | wc -l) | awk "{a=100*(1-\$1/\$3); b=100*(1-\$2/\$4); printf(\"%i\\n\", $printmode)}"
+fi
 rm "$fa" "$fb"
