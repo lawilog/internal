43a45bd440a37a96ffe8f8367161d1f82567a7f1
Lars Winterfeld <lars.winterfeld@tu-ilmenau.de>
2016-02-09 12:40:11 +0100

    rewrite benchmark.c without chunks. use vmalloc instead (or even just malloc)
    compile:
    gcc -O3 -std=c99 benchmark.c -o benchmark -lpthread
    
    example runs and example times:
    time ./benchmark 1 100 0 0    # (a) 4s
    time ./benchmark 1 400 0 0    # (b) 16s
    time ./benchmark 1 400 0 0.05 # (c) 24s
    time ./benchmark 16 400 0 0   # (d) 5s
    time ./benchmark 1 400 10 0   # (e) 7min 35s
    time ./benchmark 16 400 10 0  # (f) 36s
    
    compared to (a), (b) processes 4 times as much memory, so takes 4 times as long
    
    compared to (b), (c) has more random access (cache misses), therefore takes longer
    
    compared to (b), (d) has 16 threads, but is only about 3.5 times as fast. here we have few operations and are bandwidth limited. so more cpu does not help so much
    
    compared to (b), (e) makes much more operations per memory element, so it takes much longer
    
    compared to (e), (f) uses 16 threads and is 12.5 as fast. because we did many operations on one memory element, more cpu helps us much more than in case (d)

--- small/benchmark/benchmark.c
+++ small/benchmark/benchmark.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <time.h>
+#include <pthread.h>
+// #include <linux/vmalloc.h>
+#define vmalloc malloc
 
 #ifndef uint64_t
 typedef unsigned long long uint64_t;
@@ -9,44 +11,36 @@ typedef unsigned long long uint64_t;
 
 struct thread_data
 {
-	double** data_chunks;
-	uint64_t** ind_chunks;
+	double* X;
+	uint64_t* ind;
 	double h;
-	uint64_t my_start_ind;
-	uint64_t my_end_ind;
 	double f_shuffle;
 	unsigned n_ops;
+	uint64_t my_start_ind;
+	uint64_t my_end_ind;
 	double sum;
 };
 typedef struct thread_data thread_data;
 
-const unsigned max_chunks = 2700;
-const unsigned chunks_size = 104857600 / (sizeof(double) + sizeof(uint64_t)); // 104857600 = 100MB
-
 void* thread_init(void* vdata)
 {
-	thread_data* data = (thread_data*) vdata;
-	uint64_t chunk, elemt;
-	for(uint64_t i = data->my_start_ind; i < data->my_end_ind; ++i)
-	{
-		chunk = i / chunks_size;
-		elemt = i % chunks_size;
-		data->ind_chunks[chunk][elemt] = i;
-		data->data_chunks[chunk][elemt] = i * data->h;
-	}
-	
-	const uint64_t n_thread_elements = data->my_end_ind - data->my_start_ind;
+	thread_data* const data = (thread_data*) vdata;
+	double* const X = data -> X;
+	uint64_t* const ind = data -> ind;
+	const uint64_t my_start_ind = data -> my_start_ind;
+	const uint64_t my_end_ind = data -> my_end_ind;
+	const uint64_t n_thread_elements = my_end_ind - my_start_ind;
 	const uint64_t n_shuffle = (uint64_t)(n_thread_elements * data->f_shuffle);
-	uint64_t chunk2, elemt2;
+	for(uint64_t i = my_start_ind; i < my_end_ind; ++i)
+	{
+		ind[i] = i;
+		X[i] = i * data->h;
+	}
 	for(uint64_t kk, k = 0, s = 0; s < n_shuffle; ++s)
 	{
-		kk = data->my_start_ind + (k + 3335777) % n_thread_elements; // 3335777 is prime
-		chunk = k / chunks_size;
-		elemt = k % chunks_size;
-		chunk2 = kk / chunks_size;
-		elemt2 = kk % chunks_size;
-		data->ind_chunks[chunk][elemt]   = kk;
-		data->ind_chunks[chunk2][elemt2] = k;
+		kk = my_start_ind + (k + 3335777) % n_thread_elements; // 3335777 is prime
+		ind[k]  = kk;
+		ind[kk] = k;
 		k = kk;
 	}
 	return NULL;
@@ -54,30 +48,28 @@ void* thread_init(void* vdata)
 
 void* thread_run(void* vdata)
 {
-	thread_data* data = (thread_data*) vdata;
-	const unsigned n_ops = data->n_ops;
-	double xi, x, xx, D;
-	uint64_t chunk, elemt, ind;
-	for(uint64_t i = data->my_start_ind; i < data->my_end_ind; ++i)
+	thread_data* const data = (thread_data*) vdata;
+	double* const X = data -> X;
+	uint64_t* const ind = data -> ind;
+	const unsigned n_ops = data -> n_ops;
+	const uint64_t my_start_ind = data -> my_start_ind;
+	const uint64_t my_end_ind = data -> my_end_ind;
+	double* const sum = & data->sum;
+	for(uint64_t i = my_start_ind; i < my_end_ind; ++i)
 	{
-		chunk = i / chunks_size;
-		elemt = i % chunks_size;
-		ind = data->ind_chunks[chunk][elemt];
-		chunk = ind / chunks_size;
-		elemt = ind % chunks_size;
-		xi = data->data_chunks[chunk][elemt];
-		D = 0.0;
+		const double xi = X[ind[i]];
+		double d = 0.0;
 		for(unsigned n = 1; n < n_ops; ++n)
 		{
-			x = n;
+			double x = n;
 			while(x > 1.5)
 			{
-				xx = x / 2.0;
+				const double xx = x / 2.0;
 				x = (xx - (unsigned long)(xx) > 0.25 ) ? 3.0*x + 1.0 : xx;
 			}
-			D += x - 1.0;
+			d += x - 1.0;
 		}
-		data->sum += xi * xi + D;
+		*sum += xi * xi + d;
 	}
 	return NULL;
 }
@@ -95,43 +87,36 @@ int main(int argc, char** argv)
 		return 1;
 	}
 	const unsigned n_threads = atoi(argv[1]);
-	const unsigned n_chunks  = atoi(argv[2]);
+	const uint64_t n_mem     = atoi(argv[2]);
 	const unsigned n_ops     = atoi(argv[3]);
 	const double f_shuffle   = atof(argv[4]);
-	if(n_chunks > max_chunks)
-	{
-		printf("TotalMem must be <= %u\n", max_chunks);
-		return 1;
-	}
-	printf("%u threads, total mem %4.1f GB, calc %u, disorder %f\n", n_threads, 0.1*n_chunks, n_ops, f_shuffle);
-	
-	clock_t t0 = clock(), t1, t2;
+	printf("%u threads, %4.1f GB total mem, %u calc, %3.2f%% disorder\n", n_threads, 0.1*n_mem, n_ops, 100*f_shuffle);
 	
 	// alloc memory and init data structures
-	const uint64_t n_elements = n_chunks * chunks_size;
+	clock_t t0 = clock(), t1, t2, t3;
+	const uint64_t n_elements = n_mem * 104857600 / (sizeof(double) + sizeof(uint64_t)); // 104857600 == 100MiB
 	const uint64_t elements_per_thread = n_elements / n_threads;
 	const double h = 1.0 / (n_elements - 1);
-	double* data_chunks[max_chunks]; // static array of pointers
-	uint64_t* ind_chunks[max_chunks];
-	for(unsigned c = 0; c < n_chunks; ++c)
+	double* const X = (double*) vmalloc(sizeof(double) * n_elements);
+	uint64_t* const ind = (uint64_t*) vmalloc(sizeof(uint64_t) * n_elements);
+	pthread_t* const pthreads = (pthread_t*) malloc(sizeof(pthread_t) * n_threads);
+	thread_data* const thread_dats = (thread_data*) malloc(sizeof(thread_data) * n_threads);
+	if(!X || !ind || !pthreads || !thread_dats)
 	{
-		data_chunks[c] = (double*) malloc(sizeof(double) * chunks_size);
-		ind_chunks[c]  = (uint64_t*) malloc(sizeof(uint64_t) * chunks_size);
+		printf("ERROR: Failed to allocate enough memory.\n");
+		return 1;
 	}
-	pthread_t* pthreads = (pthread_t*) malloc(sizeof(pthread_t) * n_threads);
-	thread_data* thread_dats = (thread_data*) malloc(sizeof(thread_data) * n_threads);
-	uint64_t ind = 0;
+	uint64_t pos = 0;
 	for(unsigned t = 0; t < n_threads; ++t)
 	{
-		thread_dats[t].data_chunks = data_chunks;
-		thread_dats[t].ind_chunks = ind_chunks;
+		thread_dats[t].X = X;
+		thread_dats[t].ind = ind;
 		thread_dats[t].h = h;
-		thread_dats[t].my_start_ind = ind; ind += elements_per_thread;
-		thread_dats[t].my_end_ind   = ind; ++ind;
 		thread_dats[t].f_shuffle = f_shuffle;
 		thread_dats[t].n_ops = n_ops;
+		thread_dats[t].my_start_ind = pos; pos += elements_per_thread;
+		thread_dats[t].my_end_ind   = pos; ++pos;
 		thread_dats[t].sum = 0.0;
-		// printf("Thread %u from %llu to %llu.\n", t, thread_dats[t].my_start_ind, thread_dats[t].my_end_ind);
 	}
 	thread_dats[n_threads-1].my_end_ind = n_elements;
 	
@@ -146,6 +131,7 @@ int main(int argc, char** argv)
 	
 	// perform calculation
 	printf("Integrating... ");
+	t2 = clock();
 	for(unsigned t = 0; t < n_threads; ++t)
 		pthread_create(&pthreads[t], NULL, thread_run, & thread_dats[t]);
 	
@@ -155,21 +141,15 @@ int main(int argc, char** argv)
 	double s = 0.0;
 	for(unsigned t = 0; t < n_threads; ++t)
 		s += thread_dats[t].sum;
-	s /= n_elements;
+	s = h * (s - 0.5);
+	t3 = clock();
 	printf("%20.16f\n", s);
-	
-	t2 = clock();
+	printf("avg time per thread: %10.3f init, %10.3f calc\n", (double)(t1-t0)/CLOCKS_PER_SEC/n_threads, (double)(t3-t2)/CLOCKS_PER_SEC/n_threads);
 	
 	// free memory
 	free(thread_dats);
 	free(pthreads);
-	for(unsigned c = n_chunks; c > 0; --c)
-	{
-		free(ind_chunks[c-1]);
-		free(data_chunks[c-1]);
-	}
-	
-	printf("cpu time: %10.3f init, %10.3f calc\n", (double)(t1-t0)/CLOCKS_PER_SEC, (double)(t2-t1)/CLOCKS_PER_SEC);
-	
+	free(ind);
+	free(X);
 	return 0;
 }
---
+++ small/benchmark/benchmark_chunks.c
@@ -0,0 +1,175 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+
+#ifndef uint64_t
+typedef unsigned long long uint64_t;
+#endif
+
+struct thread_data
+{
+	double** data_chunks;
+	uint64_t** ind_chunks;
+	double h;
+	uint64_t my_start_ind;
+	uint64_t my_end_ind;
+	double f_shuffle;
+	unsigned n_ops;
+	double sum;
+};
+typedef struct thread_data thread_data;
+
+const unsigned max_chunks = 2700;
+const unsigned chunks_size = 104857600 / (sizeof(double) + sizeof(uint64_t)); // 104857600 = 100MB
+
+void* thread_init(void* vdata)
+{
+	thread_data* data = (thread_data*) vdata;
+	uint64_t chunk, elemt;
+	for(uint64_t i = data->my_start_ind; i < data->my_end_ind; ++i)
+	{
+		chunk = i / chunks_size;
+		elemt = i % chunks_size;
+		data->ind_chunks[chunk][elemt] = i;
+		data->data_chunks[chunk][elemt] = i * data->h;
+	}
+	
+	const uint64_t n_thread_elements = data->my_end_ind - data->my_start_ind;
+	const uint64_t n_shuffle = (uint64_t)(n_thread_elements * data->f_shuffle);
+	uint64_t chunk2, elemt2;
+	for(uint64_t kk, k = 0, s = 0; s < n_shuffle; ++s)
+	{
+		kk = data->my_start_ind + (k + 3335777) % n_thread_elements; // 3335777 is prime
+		chunk = k / chunks_size;
+		elemt = k % chunks_size;
+		chunk2 = kk / chunks_size;
+		elemt2 = kk % chunks_size;
+		data->ind_chunks[chunk][elemt]   = kk;
+		data->ind_chunks[chunk2][elemt2] = k;
+		k = kk;
+	}
+	return NULL;
+}
+
+void* thread_run(void* vdata)
+{
+	thread_data* data = (thread_data*) vdata;
+	const unsigned n_ops = data->n_ops;
+	double xi, x, xx, D;
+	uint64_t chunk, elemt, ind;
+	for(uint64_t i = data->my_start_ind; i < data->my_end_ind; ++i)
+	{
+		chunk = i / chunks_size;
+		elemt = i % chunks_size;
+		ind = data->ind_chunks[chunk][elemt];
+		chunk = ind / chunks_size;
+		elemt = ind % chunks_size;
+		xi = data->data_chunks[chunk][elemt];
+		D = 0.0;
+		for(unsigned n = 1; n < n_ops; ++n)
+		{
+			x = n;
+			while(x > 1.5)
+			{
+				xx = x / 2.0;
+				x = (xx - (unsigned long)(xx) > 0.25 ) ? 3.0*x + 1.0 : xx;
+			}
+			D += x - 1.0;
+		}
+		data->sum += xi * xi + D;
+	}
+	return NULL;
+}
+
+int main(int argc, char** argv)
+{
+	// parse input
+	if(argc < 4)
+	{
+		printf("usage: %s N TotalMem AccessDisorder FloatIntensity\n"
+			"N:              [integer] number of threads\n"
+			"TotalMem:       [integer] RAM in multiples of 100MB, <= %u\n"
+			"CalcIntensity:  [integer] the greater this number, the more operations per memory\n"
+			"AccessDisorder: [float] \"random\" access parameter. from 0 = ordered to 1 = disordered\n", argv[0], 2700);
+		return 1;
+	}
+	const unsigned n_threads = atoi(argv[1]);
+	const unsigned n_chunks  = atoi(argv[2]);
+	const unsigned n_ops     = atoi(argv[3]);
+	const double f_shuffle   = atof(argv[4]);
+	if(n_chunks > max_chunks)
+	{
+		printf("TotalMem must be <= %u\n", max_chunks);
+		return 1;
+	}
+	printf("%u threads, total mem %4.1f GB, calc %u, disorder %f\n", n_threads, 0.1*n_chunks, n_ops, f_shuffle);
+	
+	clock_t t0 = clock(), t1, t2;
+	
+	// alloc memory and init data structures
+	const uint64_t n_elements = n_chunks * chunks_size;
+	const uint64_t elements_per_thread = n_elements / n_threads;
+	const double h = 1.0 / (n_elements - 1);
+	double* data_chunks[max_chunks]; // static array of pointers
+	uint64_t* ind_chunks[max_chunks];
+	for(unsigned c = 0; c < n_chunks; ++c)
+	{
+		data_chunks[c] = (double*) malloc(sizeof(double) * chunks_size);
+		ind_chunks[c]  = (uint64_t*) malloc(sizeof(uint64_t) * chunks_size);
+	}
+	pthread_t* pthreads = (pthread_t*) malloc(sizeof(pthread_t) * n_threads);
+	thread_data* thread_dats = (thread_data*) malloc(sizeof(thread_data) * n_threads);
+	uint64_t ind = 0;
+	for(unsigned t = 0; t < n_threads; ++t)
+	{
+		thread_dats[t].data_chunks = data_chunks;
+		thread_dats[t].ind_chunks = ind_chunks;
+		thread_dats[t].h = h;
+		thread_dats[t].my_start_ind = ind; ind += elements_per_thread;
+		thread_dats[t].my_end_ind   = ind; ++ind;
+		thread_dats[t].f_shuffle = f_shuffle;
+		thread_dats[t].n_ops = n_ops;
+		thread_dats[t].sum = 0.0;
+		// printf("Thread %u from %llu to %llu.\n", t, thread_dats[t].my_start_ind, thread_dats[t].my_end_ind);
+	}
+	thread_dats[n_threads-1].my_end_ind = n_elements;
+	
+	// init data
+	for(unsigned t = 0; t < n_threads; ++t)
+		pthread_create(&pthreads[t], NULL, thread_init, & thread_dats[t]);
+	
+	for(unsigned t = 0; t < n_threads; ++t)
+		pthread_join(pthreads[t], NULL);
+	
+	t1 = clock();
+	
+	// perform calculation
+	printf("Integrating... ");
+	for(unsigned t = 0; t < n_threads; ++t)
+		pthread_create(&pthreads[t], NULL, thread_run, & thread_dats[t]);
+	
+	for(unsigned t = 0; t < n_threads; ++t)
+		pthread_join(pthreads[t], NULL);
+	
+	double s = 0.0;
+	for(unsigned t = 0; t < n_threads; ++t)
+		s += thread_dats[t].sum;
+	s /= n_elements;
+	printf("%20.16f\n", s);
+	
+	t2 = clock();
+	
+	// free memory
+	free(thread_dats);
+	free(pthreads);
+	for(unsigned c = n_chunks; c > 0; --c)
+	{
+		free(ind_chunks[c-1]);
+		free(data_chunks[c-1]);
+	}
+	
+	printf("cpu time: %10.3f init, %10.3f calc\n", (double)(t1-t0)/CLOCKS_PER_SEC, (double)(t2-t1)/CLOCKS_PER_SEC);
+	
+	return 0;
+}
