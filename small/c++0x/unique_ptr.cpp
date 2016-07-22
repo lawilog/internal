71966ebac375913f340f8c759038f45a1eb3c6e9
Lars Winterfeld <lars.winterfeld@tu-ilmenau.de>
2016-07-22 17:31:11 +0200

    update unique_ptr

--- small/c++0x/unique_ptr.cpp
+++ small/c++0x/unique_ptr.cpp
@@ -8,6 +8,7 @@ class Base
 	int x;
 	virtual void hello() = 0;
 	Base(int _x) : x(_x) {};
+	virtual ~Base() {cout<<"~Base()"<<endl;}
 };
 
 class A : public Base
@@ -15,7 +16,8 @@ class A : public Base
 	public:
 	int a;
 	void hello() {cout<<"Hello A "<< x << " "<< a <<endl;}
-	A(int _x, int _a): Base(_x), a(_a) {};
+	A(int _x, int _a): Base(_x), a(_a) {}
+	~A() {cout<<"~A()"<<endl;}
 };
 
 class B : public Base
@@ -23,15 +25,23 @@ class B : public Base
 	public:
 	int b;
 	void hello() {cout<<"Hello B "<< x << " "<< b <<endl;}
-	B(int _x, int _b): Base(_x), b(_b) {};
+	void onlyB() {cout<<"only B can do this"<<endl;}
+	B(int _x, int _b): Base(_x), b(_b) {}
+	~B() {cout<<"~B()"<<endl;}
 };
 
 int main()
 {
 	unique_ptr<Base> p;
-	int c; cin >> c;
+	cout<<"1? "; int c; cin >> c;
 	if(c==1) p = unique_ptr<Base>(new A(2, 3));
-	else     p = unique_ptr<Base>(new B(5, 7));
+	else
+	{
+		B* pb = new B(5, 7);
+		pb -> onlyB();
+		p = unique_ptr<B>(pb);
+		// p = move(unique_ptr<B>(pb));
+	}
 	p->hello();
 	return 0;
 }
