09ea0a94031dc1b92fdeca5ebd6013b0eb984950
Lars Winterfeld <lars.winterfeld@tu-ilmenau.de>
2016-12-05 16:34:14 +0100

    bugfix gs_embed_fonts.sh (confused input and output path)

--- little_helpers/gs_embed_fonts.sh
+++ little_helpers/gs_embed_fonts.sh
@@ -2,4 +2,4 @@
 inf="$1"
 outf="${2:-embedded_$1}"
 echo "$inf --> $outf"
-gs -q -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile="$inf" -dPDFSETTINGS=/prepress -dEmbedAllFonts=true -dSubsetFonts=true -dCompatibilityLevel=1.6 "$outf"
+gs -q -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile="$outf" -dPDFSETTINGS=/prepress -dEmbedAllFonts=true -dSubsetFonts=true -dCompatibilityLevel=1.6 "$inf"
