78f3cde3b757f5d1644712e8a6047fd21ed3dad7
Lars Winterfeld <lars.winterfeld@tu-ilmenau.de>
2020-02-02 20:34:42 +0100

    add triangulation script (and other small stuff)

--- little_helpers/randompass.sh
+++ little_helpers/randompass.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-w=${1:-8}
-n=${2:-10}
-c=${3:-'[[:alnum:]]!#$%&()*+,-./:;<=>?@[\]^_{|}'} # '[:alnum:]%+-./=_'
+w=${1:-12}
+n=${2:-8}
+c=${3:-'ABCDEFGHJLMNPQRSTWXY23456789abdefghijpqrstwxyz=+-'}
+# '[[:alnum:]]!#$%&()*+,-./:;<=>?@[\]^_{|}'
+# '[:alnum:]%+-./=_'
 cat /dev/urandom | tr -cd "$c" | fold -w "$w" | head -n "$n"
