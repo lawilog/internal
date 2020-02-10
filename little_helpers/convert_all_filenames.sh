565c256118d6ecf6f2d1f87260e8916dac20af9b
Lars Winterfeld <lw@ico-lux.de>
2020-02-10 13:38:56 +0100

    change charset trial order for filename conversion

--- little_helpers/convert_all_filenames.sh
+++ little_helpers/convert_all_filenames.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 pat="^[[:alnum:] _-.,\`'´{}()/&~]*$"
-find $@ | egrep -v "$pat" | while read p; do
+# | egrep -v "$pat"
+find $@ | while read p; do
 	d=$(dirname  "$p")
 	f=$(basename "$p")
-	for c in CP437 ISO_8859-15 IBM850; do # see iconv -l for a complete list
+	for c in ISO_8859-15 CP437 IBM850; do # see iconv -l for a complete list
 		fc=$(echo "$f" | iconv -f "$c" -t UTF-8)
 		# only if new name is good, do rename
 		echo "$fc" | egrep -q "$pat" && mv "$p" "$d/$fc" && break
