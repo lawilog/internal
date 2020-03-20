d42ce8ce7b3f1a7e80847643523e12ea6dad9075
Lars Winterfeld <lw@ico-lux.de>
2020-03-20 10:05:32 +0100

    improve convert_all_filenames.sh

--- little_helpers/convert_all_filenames.sh
+++ little_helpers/convert_all_filenames.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-pat="^[[:alnum:] _-.,\`'´{}()/&~]*$"
+pat="^[[:alnum:] _-\.,\'{}\(\)/&~]*$"
 # | egrep -v "$pat"
 find $@ | while read p; do
 	d=$(dirname  "$p")
 	f=$(basename "$p")
+	# if already good, dont try correction
+	if echo "$f" | egrep -q "$pat"; then continue; fi
+
 	for c in ISO_8859-15 CP437 IBM850; do # see iconv -l for a complete list
 		fc=$(echo "$f" | iconv -f "$c" -t UTF-8)
+		if [ "$fc" = "$f" ]; then continue; fi
 		# only if new name is good, do rename
-		echo "$fc" | egrep -q "$pat" && mv "$p" "$d/$fc" && break
+		if echo "$fc" | egrep -q "$pat"; then mv -vi "$p" "$d/$fc"; break; fi
 	done
 	test -e "$p" && echo "# $p"
 done | uniq
